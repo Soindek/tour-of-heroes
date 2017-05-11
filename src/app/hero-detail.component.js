f89d4717131706253dff398e3f5df3e4c3f69ff6
soindek <soindek@gmail.com>
2017-05-11 14:35:02 +0200

    inplement http service fully

--- src/app/hero-detail.component.js
+++ src/app/hero-detail.component.js
@@ -30,6 +30,19 @@ var HeroDetailComponent = (function () {
     HeroDetailComponent.prototype.goBack = function () {
         this.location.back();
     };
+    HeroDetailComponent.prototype.save = function () {
+        var _this = this;
+        this.heroService.update(this.hero)
+            .then(function () { return _this.goBack(); });
+    };
+    HeroDetailComponent.prototype.delFromDetail = function (hero) {
+        var _this = this;
+        this.heroService
+            .delete(hero.id)
+            .then(function () {
+            _this.goBack();
+        });
+    };
     return HeroDetailComponent;
 }());
 __decorate([
@@ -40,7 +53,7 @@ HeroDetailComponent = __decorate([
     core_1.Component({
         selector: 'hero-detail',
         templateUrl: './hero-detail.component.html',
-        styleUrls: ['./hero-detail.component.html']
+        styleUrls: ['./hero-detail.component.css']
     }),
     __metadata("design:paramtypes", [hero_service_1.HeroService,
         router_1.ActivatedRoute,
--- src/app/hero-detail.component.ts
+++ src/app/hero-detail.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from './hero.service';
 @Component({
   selector: 'hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.html']
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
@@ -30,4 +30,18 @@ export class HeroDetailComponent implements OnInit {
   goBack(): void {
     this.location.back();
   }
+
+  save(): void {
+    this.heroService.update(this.hero)
+        .then(() => this.goBack());
+  }
+
+  delFromDetail(hero: Hero): void {
+    this.heroService
+        .delete(hero.id)
+        .then(() => {
+          this.goBack();
+        });
+  }
+
 }
